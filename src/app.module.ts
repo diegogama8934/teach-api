a08e605e051fc38afa57c418c9d699eda9339881
Diego Martinez Garcia <diegogama8934@gmail.com>
2025-02-02 22:13:47 -0600

    ADD: DB connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { SubjectModule } from './subject/subject.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -14,8 +16,11 @@ import { UserModule } from './user/user.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      autoLoadEntities: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     SubjectModule,
     UserModule,
